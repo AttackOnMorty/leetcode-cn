4178c86e4995edc85bc7350b19208763680ae76a
pfmao <ErenJagerM@outlook.com>
2022-05-24 21:49:02 +0800

    Finish 25 lcof

---
+++ src/recursion/剑指 Offer 25. 合并两个排序的链表.js
@@ -0,0 +1,41 @@
+/*
+剑指 Offer 25. 合并两个排序的链表
+
+输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+
+示例1：
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+
+限制：
+0 <= 链表长度 <= 1000
+
+链接：https://leetcode.cn/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+// O(m + n) / O(m + n)
+function mergeTwoLists(l1, l2) {
+    if (l1 === null) return l2;
+    if (l2 === null) return l1;
+
+    if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
+}
