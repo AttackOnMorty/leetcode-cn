ae4acddcc7aaadad3fd268d184053a3c901723df
pfmao <ErenJagerM@outlook.com>
2022-05-26 08:46:00 +0800

    Redo 58 lcof

--- src/pure-programming/剑指 Offer 58 - II. 左旋转字符串.js
+++ src/pure-programming/剑指 Offer 58 - II. 左旋转字符串.js
@@ -1,20 +1,16 @@
 /*
 剑指 Offer 58 - II. 左旋转字符串
 
-字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。
-请定义一个函数实现字符串左旋转操作的功能。
-比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
-
+字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
 
 示例 1：
 输入: s = "abcdefg", k = 2
-输出: "cdefgab"
+输出: "cdefgab"
 
 示例 2：
 输入: s = "lrloseumgh", k = 6
-输出: "umghlrlose"
- 
-
+输出: "umghlrlose"
+ 
 限制：
 1 <= k < s.length <= 10000
 
@@ -40,13 +36,13 @@ function reverseLeftWords(s, k) {
 }
 
 // O(n) / O(1)
-function reverse(arr, start, end) {
-    while (start < end) {
-        const temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
+function reverse(arr, i, j) {
+    while (i < j) {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
 
-        start++;
-        end--;
+        i++;
+        j--;
     }
 }
