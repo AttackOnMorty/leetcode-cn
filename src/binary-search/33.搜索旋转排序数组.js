fa88b100ee5a2523553a2a8a82a0aa3745693a6b
pfmao <ErenJagerM@outlook.com>
2022-06-10 16:37:46 +0800

    Finish 33

---
+++ src/binary-search/33.搜索旋转排序数组.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=javascript
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+// O(logn) / O(1)
+function search(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = left + Math.floor((right - left) >> 1);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[left] <= nums[mid]) {
+            if (target >= nums[left] && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if (target > nums[mid] && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
+
+// @lc code=end
