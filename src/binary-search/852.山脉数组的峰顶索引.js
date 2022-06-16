7d352a6c9bdfb83e65ac32d4ffce1b857fe38ab9
pfmao <ErenJagerM@outlook.com>
2022-06-16 10:37:11 +0800

    Finish 852

---
+++ src/binary-search/852.山脉数组的峰顶索引.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=852 lang=javascript
+ *
+ * [852] 山脉数组的峰顶索引
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+
+// O(logn) / O(1)
+function peakIndexInMountainArray(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        const mid = left + ((right - left) >> 1);
+        const num = arr[mid];
+
+        if (num < arr[mid + 1]) {
+            left = mid + 1;
+        } else if (num > arr[mid + 1]) {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+}
+
+// @lc code=end
